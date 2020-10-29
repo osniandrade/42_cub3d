71627677aa208d75738326bbc9549d89a2d2a240
osni junior <osniandrade@gmail.com>
2020-10-29 10:42:32 -0300

    raycast prototype skeleton

---
+++ pikuma_test/raycasting/raycast.js
@@ -0,0 +1,12 @@
+function setup() {
+    // inicializa os objetos
+}
+
+function update() {
+    // atualiza todos os objetos antes de renderizar o proximo frame
+}
+
+function draw() {
+    update();
+    // renderiza todos os objetos frame a frame
+}
