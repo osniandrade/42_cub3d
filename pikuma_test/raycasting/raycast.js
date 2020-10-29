c84326961255e306b47cf1d65efcf277e2abb73b
osni junior <osniandrade@gmail.com>
2020-10-29 12:19:37 -0300

    mapa 2d funcionando sem colisoes

--- pikuma_test/raycasting/raycast.js
+++ pikuma_test/raycasting/raycast.js
@@ -1,12 +1,114 @@
+const TILE_SIZE = 32;
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15;
+
+const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+
+class Map {
+    constructor() {
+        this.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+    }
+    render() {
+        for (var i = 0; i < MAP_NUM_ROWS; i++) {
+            for (var j = 0; j < MAP_NUM_COLS; j++) {
+                var tileX = j * TILE_SIZE;
+                var tileY = i * TILE_SIZE;
+                var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
+                stroke("#222");
+                fill(tileColor);
+                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+            }
+        }
+    }
+}
+
+class Player {
+  constructor() {
+    this.x = WINDOW_WIDTH / 2;
+    this.y = WINDOW_HEIGHT / 2;
+    this.radius = 3;
+    this.turnDirection = 0;  // -1 if left, +1 if right
+    this.walkDirection = 0;  // -1 if back, +1 if front
+    this.rotationAngle = Math.PI / 2;
+    this.moveSpeed = 2.0;
+    this.rotationSpeed = 2 * (Math.PI / 180);  // 2 degrees per frame
+  }
+  render() {
+    noStroke();
+    fill("red");
+    circle(this.x, this.y, this.radius);
+    stroke("red");
+    line(
+      this.x, 
+      this.y,
+      this.x + Math.cos(this.rotationAngle) * 20,
+      this.y + Math.sin(this.rotationAngle) * 20
+    );
+  }
+  update() {
+    // atualiza posição do player baseado em turnDirection e walkDirection
+    this.rotationAngle += this.turnDirection * this.rotationSpeed;
+
+    var moveStep = this.walkDirection * this.moveSpeed;
+    
+    this.x += Math.cos(this.rotationAngle) * moveStep;
+    this.y += Math.sin(this.rotationAngle) * moveStep;
+  }
+}
+
+var grid = new Map();
+var player = new Player();
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    player.walkDirection = +1;
+  } else if (keyCode == DOWN_ARROW) {
+    player.walkDirection = -1;
+  } else if (keyCode == RIGHT_ARROW) {
+    player.turnDirection = +1;
+  } else if (keyCode == LEFT_ARROW) {
+    player.turnDirection = -1;
+  }
+}
+
+function keyReleased() {
+  if (keyCode == UP_ARROW) {
+    player.walkDirection = 0;
+  } else if (keyCode == DOWN_ARROW) {
+    player.walkDirection = 0;
+  } else if (keyCode == RIGHT_ARROW) {
+    player.turnDirection = 0;
+  } else if (keyCode == LEFT_ARROW) {
+    player.turnDirection = 0;
+  }
+}
+
 function setup() {
     // inicializa os objetos
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT)
 }
 
 function update() {
     // atualiza todos os objetos antes de renderizar o proximo frame
+    player.update();
 }
 
 function draw() {
-    update();
     // renderiza todos os objetos frame a frame
+    update();
+    grid.render();
+    player.render();
 }
