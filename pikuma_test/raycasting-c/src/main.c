625ccf13268608b5300f8827e00c8083f187b11c
osni junior <osniandrade@gmail.com>
2020-11-24 10:12:08 -0300

    starting C project, window draw and moving object already done

---
+++ pikuma_test/raycasting-c/src/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int isGameRunning = FALSE;
+
+int playerX, playerY;
+
+int initializeWindow() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    }
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window. \n");
+        return FALSE;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);  // -1 gets the default driver
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+    }
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    
+    return TRUE;
+}
+
+void destroyWindow() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void setup() {
+    playerX = 0;
+    playerY = 0;
+}
+
+void processInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT: {
+            isGameRunning = FALSE;
+            break;
+        }
+        case SDL_KEYDOWN: {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                isGameRunning = FALSE;
+            break;
+        }
+    }
+}
+
+void update() {
+    playerX += 1;
+    playerY += 1;
+}
+
+void render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_Rect rect = {playerX, playerY, 20, 20};
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+}
+
+int main () {
+    isGameRunning = initializeWindow();
+
+    setup();
+
+    while(isGameRunning) {
+        processInput();
+        update();
+        render();
+    }
+
+    destroyWindow;
+
+    return 0;
+}
