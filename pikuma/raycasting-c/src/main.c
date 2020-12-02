b66db1fbd920db149acb07465290746dd8612092
osni junior <osniandrade@gmail.com>
2020-12-02 10:56:41 -0300

    pikuma project rendering map

--- pikuma/raycasting-c/src/main.c
+++ pikuma/raycasting-c/src/main.c
@@ -2,12 +2,39 @@
 #include <SDL2/SDL.h>
 #include "constants.h"
 
+struct Player {
+    float x;
+    float y;
+    float width;
+    float height;
+    int turnDirection;  // -1 for left, +1 for right
+    int walkDirection;  // -1 for back, +1 for front
+    float rotationAngle;
+    float walkSpeed;
+    float turnSpeed;
+} player;
+
+const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 int isGameRunning = FALSE;
 int ticksLastFrame = 0;
 
-int playerX, playerY;
 
 int initializeWindow() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -42,8 +69,34 @@ void destroyWindow() {
 }
 
 void setup() {
-    playerX = 0;
-    playerY = 0;
+    player.x = WINDOW_WIDTH / 2;
+    player.y = WINDOW_HEIGHT / 2;
+    player.width = 5;
+    player.height = 5;
+    player.turnDirection = 0;
+    player.walkDirection = 0;
+    player.rotationAngle = PI / 2;
+    player.walkSpeed = 100;
+    player.turnSpeed = 45 * (PI / 180);
+}
+
+void renderMap() {
+    for (int i = 0; i < MAP_NUM_ROWS; i++) {
+        for (int j = 0; j < MAP_NUM_COLS; j++) {
+            int tileX = j * TILE_SIZE;
+            int tileY = i * TILE_SIZE;
+            int tileColor = map[i][j] != 0 ? 255 : 0;
+
+            SDL_SetRenderDrawColor(renderer, tileColor, tileColor, tileColor, 255);
+            SDL_Rect mapTileRect = {
+                tileX * MINIMAP_SCALE_FACTOR,
+                tileY * MINIMAP_SCALE_FACTOR,
+                TILE_SIZE * MINIMAP_SCALE_FACTOR,
+                TILE_SIZE * MINIMAP_SCALE_FACTOR
+            };
+            SDL_RenderFillRect(renderer, &mapTileRect);
+        }
+    }
 }
 
 void processInput() {
@@ -63,24 +116,28 @@ void processInput() {
 }
 
 void update() {
-    // waste some time until we reach the target frame time length
-    while(!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TIME_LENGTH));
+    // compute how long we have until we reach the target frame time in miliseconds
+    int timeToWait = FRAME_TIME_LENGTH - (SDL_GetTicks() - ticksLastFrame);
 
+    // only delay execution if we are running too fast
+    if (timeToWait > 0 && timeToWait <= FRAME_TIME_LENGTH) {
+        SDL_Delay(timeToWait);
+    }
+    
     float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f; // calculates DeltaTime
 
     ticksLastFrame = SDL_GetTicks();
 
-    playerX += 50 * deltaTime;
-    playerY += 50 * deltaTime;
+    // TODO: update game objects as a function of deltaTime
 }
 
 void render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_Rect rect = {playerX, playerY, 20, 20};
-    SDL_RenderFillRect(renderer, &rect);
+    renderMap();
+    //renderRays();
+    //renderPlayer();
 
     SDL_RenderPresent(renderer);
 }
