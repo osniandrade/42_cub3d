c8e1d40a24093a289be91955975f4b88ef4f6d18
osni andrade <osniandrade@gmail.com>
2021-02-11 11:52:52 -0300

    pikuma loading textures from textures.h

--- pikuma/raycasting-c/src/main.c
+++ pikuma/raycasting-c/src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include "constants.h"
 #include <limits.h>
+#include "constants.h"
+#include "textures.h"
 
 struct Player {
     float x;
@@ -29,19 +30,19 @@ struct Ray {
 } rays[NUM_RAYS];
 
 const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    {1, 0, 0, 0, 2, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5},
+    {1, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5},
+    {1, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 5},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5}
 };
 
 SDL_Window* window = NULL;
@@ -50,10 +51,8 @@ int isGameRunning = FALSE;
 int ticksLastFrame = 0;
 
 Uint32* colorBuffer = NULL;
-
 SDL_Texture* colorBufferTexture;
-
-Uint32* wallTexture = NULL;
+Uint32* textures[NUM_TEXTURES];
 
 int initializeWindow() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -112,13 +111,15 @@ void setup() {
         WINDOW_HEIGHT
     );
 
-    // create a texture with a pattern of blue and black lines
-    wallTexture = (Uint32*) malloc(sizeof(Uint32) * (Uint32)TEXTURE_WIDTH * (Uint32)TEXTURE_HEIGHT);
-    for (int x = 0; x < TEXTURE_WIDTH; x++) {
-        for (int y = 0; y < TEXTURE_HEIGHT; y++) {
-            wallTexture[(TEXTURE_WIDTH * y) + x] = (x % 8 && y % 8) ? 0xFF0000FF : 0xFF000000;
-        }
-    }
+    // load textures from textures.h
+    textures[0] = (Uint32*) REDBRICK_TEXTURE;
+    textures[1] = (Uint32*) PURPLESTONE_TEXTURE;
+    textures[2] = (Uint32*) MOSSYSTONE_TEXTURE;
+    textures[3] = (Uint32*) GRAYSTONE_TEXTURE;
+    textures[4] = (Uint32*) COLORSTONE_TEXTURE;
+    textures[5] = (Uint32*) BLUESTONE_TEXTURE;
+    textures[6] = (Uint32*) WOOD_TEXTURE;
+    textures[7] = (Uint32*) EAGLE_TEXTURE;
 }
 
 int mapHasWallAt(float x, float y) {
@@ -420,13 +421,16 @@ void generate3DProjection() {
             textureOffsetX = (int)rays[i].wallHitX % TILE_SIZE;
         }
 
+        // get the correct texture id number from map content
+        int texNum = rays[i].wallHitContent - 1;
+
         for (int y = wallTopPixel; y < wallBottomPixel; y++) {
             // calculate textureOffsetY
             int distanceFromTop = (y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2));
             int textureOffsetY = distanceFromTop * ((float)TEXTURE_HEIGHT / wallStripHeight);
 
             // set the color of the wall based on the color from the texture
-            Uint32 texelColor = wallTexture[(TEXTURE_WIDTH * textureOffsetY) + textureOffsetX];
+            Uint32 texelColor = textures[texNum][(TEXTURE_WIDTH * textureOffsetY) + textureOffsetX];
             colorBuffer[(WINDOW_WIDTH * y) + i] = texelColor;
         }
 
