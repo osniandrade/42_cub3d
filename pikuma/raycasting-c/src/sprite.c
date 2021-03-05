b63e806d57c1bb41fd8e2600bd4273d824adbff9
osni junior <osniandrade@gmail.com>
2021-03-05 17:51:46 -0300

    pikuma course update

--- pikuma/raycasting-c/src/sprite.c
+++ pikuma/raycasting-c/src/sprite.c
@@ -6,10 +6,18 @@ static sprite_t sprites[NUM_SPRITES] = {
     {.x = 640, .y = 630, .texture = 9}  // barrel
 };
 
-void renderSpriteProjection(void) {
-    // TODO:
+void renderMapSprites(void){
+    for (int i = 0; i < NUM_SPRITES; i++) {
+        drawRect(
+            sprites[i].x * MINIMAP_SCALE_FACTOR,
+            sprites[i].y * MINIMAP_SCALE_FACTOR, 
+            2, 
+            2, 
+            0xFF00FFFF);
+    }
 }
 
-void renderMapSprites(void){
-    // TODO;
+void renderSpriteProjection(void) {
+    // TODO
 }
+
