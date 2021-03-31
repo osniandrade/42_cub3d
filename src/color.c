d8de4c98b0640feb756be7072b73e39f8026a2bd
osni junior <osniandrade@gmail.com>
2021-03-31 11:06:30 -0300

    norminette check

--- src/color.c
+++ src/color.c
@@ -6,46 +6,46 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 16:09:30 by ocarlos-          #+#    #+#             */
-/*   Updated: 2021/03/30 17:07:00 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2021/03/31 11:06:29 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
 /*
-** t_cl	ft_crt_trgb(int t, int r, int g, int b)
+** T_CL	ft_crt_trgb(int t, int r, int g, int b)
 **      returns a color in hexadecimal format to use in minilibx
-** t_cl	ft_get_t(int trgb)
+** T_CL	ft_get_t(int trgb)
 **      gets a color in minilibx format and returns its transparency
-** t_cl	ft_get_r(int trgb)
+** T_CL	ft_get_r(int trgb)
 **      gets a color in minilibx format and returns its red value
-** t_cl	ft_get_g(int trgb)
+** T_CL	ft_get_g(int trgb)
 **      gets a color in minilibx format and returns its green value
-** t_cl	ft_get_b(int trgb)
+** T_CL	ft_get_b(int trgb)
 **      gets a color in minilibx format and returns its blue value
 */
 
-t_cl	ft_crt_trgb(int t, int r, int g, int b)
+T_CL	ft_crt_trgb(int t, int r, int g, int b)
 {
 	return(t << 24 | r << 16 | g << 8 | b);
 }
 
-t_cl	ft_get_t(int trgb)
+T_CL	ft_get_t(int trgb)
 {
 	return (trgb & (0xFF << 24));
 }
 
-t_cl	ft_get_r(int trgb)
+T_CL	ft_get_r(int trgb)
 {
 	return (trgb & (0xFF << 16));
 }
 
-t_cl	ft_get_g(int trgb)
+T_CL	ft_get_g(int trgb)
 {
 	return (trgb & (0xFF << 8));
 }
 
-t_cl	ft_get_b(int trgb)
+T_CL	ft_get_b(int trgb)
 {
 	return (trgb & 0xFF);
 }
