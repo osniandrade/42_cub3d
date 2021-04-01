33bab00aed2a3e83dea2165f8f78c0172c0b78af
osni junior <osniandrade@gmail.com>
2021-04-01 17:31:17 -0300

    norm

--- src/init2.c
+++ src/init2.c
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 16:09:30 by ocarlos-          #+#    #+#             */
-/*   Updated: 2021/04/01 17:30:36 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2021/04/01 17:31:12 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,18 +60,18 @@ int		ft_clear_colorbuffer(t_data *d, int init)
 	return (TRUE);
 }
 
-// void	ft_init_drwstruct(t_drwline *c, t_pos *i_pos, t_pos *f_pos)
-// {
-// 	c->i_x = round(i_pos->x);
-// 	c->i_y = round(i_pos->y);
-// 	c->f_x = round(f_pos->x);
-// 	c->f_y = round(f_pos->y);
-// 	c->dx = abs((int)c->f_x - (int)c->i_x);
-// 	c->sx = c->i_x < c->f_x ? 1 : -1;
-// 	c->dy = abs((int)c->f_y - (int)c->i_y);
-// 	c->sy = c->i_y < c->f_y ? 1 : -1;
-// 	c->err = (c->dx > c->dy ? c->dx : -c->dy) / 2;
-// }
+void	ft_init_drwstruct(t_drwline *c, t_pos *i_pos, t_pos *f_pos)
+{
+	// c->i_x = round(i_pos->x);
+	// c->i_y = round(i_pos->y);
+	// c->f_x = round(f_pos->x);
+	// c->f_y = round(f_pos->y);
+	// c->dx = abs((int)c->f_x - (int)c->i_x);
+	// c->sx = c->i_x < c->f_x ? 1 : -1;
+	// c->dy = abs((int)c->f_y - (int)c->i_y);
+	// c->sy = c->i_y < c->f_y ? 1 : -1;
+	// c->err = (c->dx > c->dy ? c->dx : -c->dy) / 2;
+}
 
 void	ft_initsprproj(t_data *d, t_pjspr *spr, int i)
 {
