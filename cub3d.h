d6e7a82f1cdab73e460f226921673fa42503af38
osni junior <osniandrade@gmail.com>
2021-04-12 11:18:46 -0300

    norm test

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 08:55:43 by ocarlos-          #+#    #+#             */
-/*   Updated: 2021/04/12 11:18:26 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2021/04/12 11:18:40 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -169,8 +169,8 @@ typedef struct	s_pjspr
 	//int			xofst;
 	//int			yofst;
 	//int			dsttop;
-	//int			t_pos;
-	//float		h;
+	int			t_pos;
+	float		h;
 	float		w;
 	float		topy;
 	float		btmy;
